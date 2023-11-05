e2c6737051837808b140f0f5fcff43b566a78b37
xero-lib <jonathankurtz@protonmail.com>
2023-11-05 12:30:06 -0600

    main: converted to questionmark notation

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 mod pieces;
 mod board;
 
-use std::io::Error;
-
 use board::*;
 
-fn main() -> Result<(), Error>{
+fn main() -> Result<(), &'static str>{
     let mut board = Board::new();
     println!("{board}");
     board.init("");
     println!("{board}");
-    board.r#move("A2", "A3").unwrap();
+    board.r#move("A2", "A3")?;
     println!("{board}");
+    board.r#move("A3", "A4")?;
+    print!("{board}");
+    board.r#move("A4", "B5")?;
     Ok(())
 }
